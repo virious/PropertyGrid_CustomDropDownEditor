39b150c41b6144fc07076471bc2b97a62e67d8da
unknown <User@User-Komputer.(none)>
2013-03-04 22:12:54 +0100

    Removed unused usings.

--- PropertyGrid_ListBox_Test/TypeCodeSelectionControl.cs
+++ PropertyGrid_ListBox_Test/TypeCodeSelectionControl.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using System.Windows.Forms.Design;
 
@@ -134,6 +129,5 @@ namespace PropertyGrid_ListBox_Test
                 editorService.CloseDropDown();
             }
         }
-
     }
 }
